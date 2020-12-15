bafcc747091154d06b714e8f273cfaff0142770c
NAF-FLY <vadim192000@gmail.com>
2020-12-16 00:53:27 +0300

    Add FragmentMoviesList.kt and FragmentMoviesDetails.kt Delete activity_movie_details.xml

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.example.movie
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), FragmentMoviesList.MoviesListListener, FragmentMoviesDetails.MovieDetailListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.btn_first)
-        button.setOnClickListener { moveToMovieDetailScreen() }
+        if(savedInstanceState==null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_main, FragmentMoviesList())
+                .commit()
+        }
     }
 
-    private fun moveToMovieDetailScreen() {
-        val intent = Intent(this, MovieDetailsActivity::class.java)
-        startActivity(intent)
+    override fun toMovieDetail() {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .add(R.id.fragment_main, FragmentMoviesDetails())
+            .commit()
     }
+
+    override fun toMoviesList() {
+        supportFragmentManager.popBackStack()
+    }
+
 }
+
+
